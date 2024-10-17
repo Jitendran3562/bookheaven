3ff63212ec0a5a0e884cb0979decc0035f815cd3
jitendra <jitendranamdev400@gmail.com>
2024-10-17 17:42:43 +0530

    update code

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bookRoute from "./route/book.route.js";
 import cors from "cors";
-import userRoute from "./route/user.route.js"
-
+import userRoute from "./route/user.route.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
 dotenv.config();
+
+const corsOptions = {
+  origin: "https://majestic-moxie-73cf6d.netlify.app",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 // connect to mongodb
 try {
-  mongoose.connect(URI,{
+  mongoose.connect(URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-    
-  );
+    useUnifiedTopology: true,
+  });
   console.log("connect to mongodb");
 } catch (error) {
   console.log("Error", error);
@@ -29,7 +30,7 @@ try {
 
 // defining route
 app.use("/book", bookRoute);
-app.use("/user", userRoute)
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
